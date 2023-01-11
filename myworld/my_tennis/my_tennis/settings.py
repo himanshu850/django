f246b5e35df827494119713cdd46e54834d4740f
Himanshu <himanshupparmar824@gmail.com>
2023-01-11 17:04:09 +0300

    second commit

--- myworld/my_tennis/my_tennis/settings.py
+++ myworld/my_tennis/my_tennis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6s)ect5@^g=k*k+$@%$6#*6ek^*t4381(&0tw(7liutj(5)i8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
